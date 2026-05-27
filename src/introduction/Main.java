35f5afe60da1d33baa9fa2688d3f37e9737eeae7
clarence <260881283+clarenceio@users.noreply.github.com>
2026-05-27 23:42:44 +0000

    init java basics

---
+++ src/introduction/Main.java
@@ -0,0 +1,7 @@
+package introduction;
+
+public class Main {
+    public static void main () {
+        System.out.println("hello, world!");
+    }
+}
---
+++ src/introduction/readme.md
@@ -0,0 +1,4 @@
+Introduction to Java Programming ...
+Anatomy of a Java Program
+
+Smallest building blocks in java programs are Functions.
