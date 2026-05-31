0afae8c755e8b008e7e236c477fa889ce7a298a9
clarence <260881283+clarenceio@users.noreply.github.com>
2026-05-31 07:35:52 +0000

    initial syntax for arrays

--- src/types/Array.java
+++ src/types/Array.java
@@ -2,17 +2,18 @@ package types;
 
 import java.util.Arrays;
 
-public class Main {
+public class Array {
     public static void main (String[] args) {
         int [] numbers = new int[5];   // use new - arrays are reference types. [5] - nnumber of elements in the arrays (size or length of the array)
         // accessing individual elements using index
         numbers[0] = 1;
         numbers[1] = 3;
 //        numbers[10] = 4;   // exception is raised as index is out of scope for length of 5.
-           //implementing a method multiple times - method overloading
-//        System.out.println(numbers);  //returns a string calculated based on the address of object in memory. each object is in a different memory space
-        System.out.println(Arrays.toString(numbers));
+           //implementing a method multiple times - method overloading - float method on toString
 
-        // initializing an array with items ahead of time
+
+//        System.out.println(numbers);  //returns a string calculated based on the address of object in memory. each object is in a different memory space
+        System.out.println(Arrays.toString(numbers));  //use the Array class with the toString method, to see the actual elements of the array
+
     }
 }
