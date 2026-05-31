1e7508d5e7f1f4fde1b49543683c9cc61f4bea26
clarence <260881283+clarenceio@users.noreply.github.com>
2026-05-31 07:43:09 +0000

    add new method for initializing arrays

--- src/types/Array.java
+++ src/types/Array.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Array {
     public static void main (String[] args) {
-        int [] numbers = new int[5];   // use new - arrays are reference types. [5] - nnumber of elements in the arrays (size or length of the array)
+    /*    int [] numbers = new int[5];   // use new - arrays are reference types. [5] - nnumber of elements in the arrays (size or length of the array)
         // accessing individual elements using index
         numbers[0] = 1;
         numbers[1] = 3;
@@ -14,6 +14,11 @@ public class Array {
 
 //        System.out.println(numbers);  //returns a string calculated based on the address of object in memory. each object is in a different memory space
         System.out.println(Arrays.toString(numbers));  //use the Array class with the toString method, to see the actual elements of the array
+*/
+        // new method for initializing arrays with known elements
 
+        int [] numbers = {2, 3, 5, 1, 4};
+        System.out.println(numbers.length);  //fixed length
+        System.out.println(Arrays.toString(numbers));
     }
 }
