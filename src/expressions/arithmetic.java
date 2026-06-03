04d5e2df4a669196c7694a184c929e75e210a9b5
clarence <260881283+clarenceio@users.noreply.github.com>
2026-06-03 07:26:19 +0000

    basic expressions in java

---
+++ src/expressions/arithmetic.java
@@ -0,0 +1,19 @@
+package expressions;
+
+public class arithmetic {
+    public static void main (String[] args) {
+        int result = 10 + 3;
+        System.out.println(result);
+
+        int subt = 10 - 3;
+        System.out.println(subt);
+
+        int mult = 10 + 3;
+        System.out.println(mult);
+
+        int div = 10 / 3;   //division of two whole numbers is a whole number
+        double divi = (double) 10 / (double) 3;
+        System.out.println(divi);
+
+    }
+}
---
+++ src/expressions/readme.md
@@ -0,0 +1,10 @@
+Java has same arithmetic operators in math
+addition +
+subtraction - 
+multiplication *
+division /
+modulo %
+
+operands are the numbers
+
+Expression is a piece of code that produces a value
