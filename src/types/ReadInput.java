6deb6947c3f2278b20e937894180f8e7c0368475
clarence <260881283+clarenceio@users.noreply.github.com>
2026-06-05 18:26:21 +0000

    read input from terminal

--- src/types/ReadInput.java
+++ src/types/ReadInput.java
@@ -1,4 +1,12 @@
 package types;
 
+import java.util.Scanner;
+
 public class ReadInput {
+    public static void main (String[] args) {
+            // specify where data is to be read, terminal window or a file
+        Scanner scanner = new Scanner(System.in);   //(system.in) - a field (more of a variable defined in class || reading from a terminal
+        byte age = scanner.nextByte();
+        System.out.println("You are " + age); //implicit casting - java converts byte var to string so they can be added together
+    }
 }
