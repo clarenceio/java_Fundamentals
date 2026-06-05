a2549f3e2e5a0f76b2fbd4a8c2313a0c49948b2a
clarence <260881283+clarenceio@users.noreply.github.com>
2026-06-05 16:34:00 +0000

    the random method

--- src/types/MathClass.java
+++ src/types/MathClass.java
@@ -25,8 +25,21 @@ public class MathClass {
         int newMin = Math.min(45, 56);  // has implementation for int, long, float, double
         System.out.println(newMin);
 
+//        random method - generates a random variable b/n 0 and 1. returns a double
+        double randomNum = Math.random();
+        randomNum = Math.random() * 100;   // numbers b/n 0 and 100
 
+            // get rid of the digits after the decimals
+        randomNum = Math.round(Math.random() * 100);
+        System.out.println(randomNum);
+                //or
+        int randomNumber = (int) Math.round(Math.random() * 100);  // change type to an int, however implicit casting cannot happen a there's a value represented in 8
+        //bytes of memory to be stored in a variable with 4 bytes of memory, hence the use of explicit casting.
+        // and the round method returns a long
+        System.out.println(randomNumber);
 
-
+        int zeroRes = (int) Math.random() * 100;
+        System.out.println(zeroRes); // returns 0. casting the round method to an int losses the fraction = 0 * 100
+        zeroRes = (int) (Math.random() * 100);
     }
 }
