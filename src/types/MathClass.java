105a2e3544e3954bf6fc44ea6d67a3edff0e2ed8
clarence <260881283+clarenceio@users.noreply.github.com>
2026-06-05 16:07:02 +0000

    the floor method

--- src/types/MathClass.java
+++ src/types/MathClass.java
@@ -10,9 +10,16 @@ public class MathClass {
         long res = Math.round(1.3);   // second option in round - takes a double and returns a long
         System.out.println(res);
 
-//    ceil - returns the smallest integer that is greater than or equal to the given value. This method returns a double
+//        ceil - returns the smallest integer that is greater than or equal to the given value. This method returns a double
         int newVal = (int)Math.ceil(2.3F); //explicit casting in action
         System.out.println(newVal);
+
+//        floor method - returns the largest number that is smaller or equal to the given value
+        int newValue = (int)Math.floor(2.3F); //explicit casting in action
+        System.out.println(newValue); // returns 2
+
+
+
 
     }
 }
