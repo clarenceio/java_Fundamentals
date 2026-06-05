c13eda955c910dd5cd52b36b41199c311b74e45b
clarence <260881283+clarenceio@users.noreply.github.com>
2026-06-05 16:02:17 +0000

    the ceil method

--- src/types/MathClass.java
+++ src/types/MathClass.java
@@ -2,6 +2,7 @@ package types;
 
 public class MathClass {
     public static void main (String[] args) {
+//        round method
         int result = Math.round(1.1F); // round a floating point number to a whole number
         // takes a float and returns an in
         System.out.println(result);
@@ -9,6 +10,9 @@ public class MathClass {
         long res = Math.round(1.3);   // second option in round - takes a double and returns a long
         System.out.println(res);
 
+//    ceil - returns the smallest integer that is greater than or equal to the given value. This method returns a double
+        int newVal = (int)Math.ceil(2.3F); //explicit casting in action
+        System.out.println(newVal);
 
     }
 }
