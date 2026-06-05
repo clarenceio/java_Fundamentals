e5d3cc64cfc304a7ec4f0ba013b46dcf2cf496d4
clarence <260881283+clarenceio@users.noreply.github.com>
2026-06-05 15:58:03 +0000

    the round method

--- src/types/MathClass.java
+++ src/types/MathClass.java
@@ -2,6 +2,13 @@ package types;
 
 public class MathClass {
     public static void main (String[] args) {
+        int result = Math.round(1.1F); // round a floating point number to a whole number
+        // takes a float and returns an in
+        System.out.println(result);
+
+        long res = Math.round(1.3);   // second option in round - takes a double and returns a long
+        System.out.println(res);
+
 
     }
 }
