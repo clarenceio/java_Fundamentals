dfb2d0729ddff37fb7502c9fc3752a48631b1b9c
clarence <260881283+clarenceio@users.noreply.github.com>
2026-06-07 12:33:02 +0000

    the or operator

--- src/controlFlow/LogicalOperators.java
+++ src/controlFlow/LogicalOperators.java
@@ -5,5 +5,11 @@ public class LogicalOperators {
         int temperature = 22;
         boolean isWarm = temperature > 20 && temperature < 30;  // both conditions must be true
         System.out.println(isWarm);
+
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean isEligible = hasHighIncome || hasGoodCredit;   // at least one condition must be true
+        System.out.println(isEligible);
+
     }
 }
