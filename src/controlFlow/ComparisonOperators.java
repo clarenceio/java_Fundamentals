ed5f198e0801fe4a7f31ab6d84a87a94e0d0d301
clarence <260881283+clarenceio@users.noreply.github.com>
2026-06-07 12:13:21 +0000

    comparison operators in java

---
+++ src/controlFlow/ComparisonOperators.java
@@ -0,0 +1,15 @@
+package controlFlow;
+
+public class ComparisonOperators {
+    public static void main (String[] args) {
+//        comparison operators  - to compare primitive types
+        int x = 1;
+        int y = 2;
+
+        System.out.println(x == y);     // equality operator
+        System.out.println(x != y);    //  inequality operator
+        System.out.println(x <= y);   //   less or equal to
+        System.out.println(x >= y);  //    greater than or equal to
+    }
+
+}
